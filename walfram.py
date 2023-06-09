cdae5b216edea0585f35edbf7a8c333ddf2d0597
nagavenkataravitejaungarala <130119502+nagavenkataravitejaungarala@users.noreply.github.com>
2023-06-09 21:28:15 +0530

    Update walfram.py

--- walfram.py
+++ walfram.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import requests
+from PIL import Image
+logo = Image.open('logo.png')
+#pip install pandas numpy matplotlib seaborn streamlit
+#to run strealit :   streamlit run test2.py 
+st.set_page_config(page_title="WALFRAMALPHA", page_icon=":bar_chart:", layout="wide")
+st.image(logo)
+# Define the list of names
+names = ["21A21A6159-U.N.V RAVITEJA"]
+st.title("WALFRAMALPHA")
+# Add the names to the sidebar
+st.sidebar.title("Project Team Members:")
+
+for name in names:
+    st.sidebar.write(name)
+
 
 # Define your Wolfram Alpha app ID
 WOLFRAM_APP_ID = 'GWA72H-LYA2A5LG4V'
