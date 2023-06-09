87f7111adab1fcf406bb9127f6ae4de1c7b0027c
nagavenkataravitejaungarala <130119502+nagavenkataravitejaungarala@users.noreply.github.com>
2023-06-09 21:29:21 +0530

    Update walfram.py

--- walfram.py
+++ walfram.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import requests
 from PIL import Image
-logo = Image.open('logo.png')
+#logo = Image.open('logo.png')
 #pip install pandas numpy matplotlib seaborn streamlit
 #to run strealit :   streamlit run test2.py 
 st.set_page_config(page_title="WALFRAMALPHA", page_icon=":bar_chart:", layout="wide")
-st.image(logo)
+#st.image(logo)
 # Define the list of names
 names = ["21A21A6159-U.N.V RAVITEJA"]
 st.title("WALFRAMALPHA")
